35de13f46cf04821b558d86b6de44c097618efe3
Kevin <kevinlamdata@gmail.com>
2022-12-20 18:02:25 -0500

    Add base covnet model

--- model/SplitData.py
+++ model/SplitData.py
@@ -9,7 +9,7 @@ data = os.listdir(folder_path)
 splitfolders.ratio(folder_path, # The location of dataset
                    output="../data/COVID-19_Radiography_Dataset/TwoClasses/split", # The output location
                    seed=42, # The number of seed
-                   ratio=(.6, .2, .2), # The ratio of splited dataset
+                   ratio=(.6, .2, .2), # The ratio of split dataset
                    group_prefix=None, # If your dataset contains more than one file like ".jpg", ".pdf", etc
                    move=False # If you choose to move, turn this into True
                    )
